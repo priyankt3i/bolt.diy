bc4dafa1de8e98cbac257b87e4837ef54bb6ee7e
Kumar Priyank <kpriyank88@gmail.com>
2025-03-04 16:43:58 +0000

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -100,6 +100,7 @@ export default defineConfig((config) => {
       config.mode !== 'test' && remixCloudflareDevProxy(),
       remixVitePlugin({
         future: {
+          v3_singleFetch: true,
           v3_fetcherPersist: true,
           v3_relativeSplatPath: true,
           v3_throwAbortReason: true,
